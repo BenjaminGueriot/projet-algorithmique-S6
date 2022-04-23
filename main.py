3a53c294a354c33d1dc0fa16213decbc5c428243
BenjaminGueriot <gueriot.benjamin@gmail.com>
2022-04-23 20:56:28 +0200

    Main file

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 #-*-coding:utf-8-*-
 from datetime import datetime
+import data2py as d2p
 import Method
 import CreateArret as CA
 from Arret import Arret
 from Lignes import Lignes
 
+lst_files = ['1_Poisy-ParcDesGlaisins.txt','2_Piscine-Patinoire_Campus.txt']
 
-dic_arret = CA.creation_arrets()
-dic_ligne = CA.creation_lignes()
+dic_arret ={}
+nb_ligne = 0
+for files in lst_files:
+    nb_ligne += 1
+    data = d2p.read_data(files)
+    dic = CA.creation_arrets(data, dic_arret,nb_ligne)
+    dic_arret = dic["dic_arret"]
+    dic_ligne = dic["dic_ligne"]
 
+
+
+
+
+#print((dic_arret.get("GARE")).get_lst_arret_suivant()[0])
 #print(((dic_arret.get("GARE")).get_lst_arret_suivant()[0]).get_nom())
 #print(((dic_arret.get("GARE")).get_lst_arret_suivant()[1]).get_nom())
 #print(dic_arret.get("GARE").get_horaire("special_go",1))
