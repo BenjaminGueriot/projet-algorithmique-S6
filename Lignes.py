f9ee972eea127d1fb63d5aac832dd3c940beb279
BenjaminGueriot <gueriot.benjamin@gmail.com>
2022-04-23 03:27:10 +0200

    Classe Lignes

--- Lignes.py
+++ Lignes.py
@@ -29,4 +29,49 @@ class Lignes:
     def add_Lst_Arret(self,Arret):
         self.Lst_Arret.append(Arret)
 
-    
+    def display_depth(self,dic_ligne,arretDebut,arretFin):
+        
+        """
+        Méthode pour réaliser une recherche en profondeur 
+        
+        Parameters
+        ----------
+        forest : RTree[]
+            Arborescences qu'il reste à explorer
+        nodes : Node[]
+            Noeuds trouvés
+            
+        Returns
+        ----------
+        nodes : Node[]
+        """
+
+        count_route = 0
+        dic_route = {}
+        start = 0
+        lst_routes = []
+
+        def dfs(forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes):
+            if forest:
+                ligne = forest[0]
+                if(ligne.ArretDepart == arretDebut):
+                    start = 1
+                if ligne.ArretDepart != arretFin and ligne.ArretDepart.is_leaf():
+                    new_forest = forest[1:]
+                    dfs(new_forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
+                else :
+                    if start == 1:
+                        lst_routes.append(ligne.ArretDepart.nom)
+
+                    if ligne.ArretDepart == arretFin:
+                        count_route += 1
+                        dic_route["Route"+str(count_route)] = lst_routes
+                        lst_routes = []
+                        new_forest = forest[1:]
+                    else :
+                        new_forest = [Lignes(arret, self.Lst_Arret) for arret in ligne.ArretDepart.get_lst_arret_suivant()] + forest[1:]
+
+                    dfs(new_forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
+
+        dfs([self],dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
+        print(dic_route)
