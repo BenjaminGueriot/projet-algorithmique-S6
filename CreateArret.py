a8865302dd7686975f8d76c44b7f6874533c0a3d
BenjaminGueriot <gueriot.benjamin@gmail.com>
2022-04-25 20:56:47 +0200

    Creating Arret and Lignes objects

--- CreateArret.py
+++ CreateArret.py
@@ -10,6 +10,7 @@ def creation_arrets(data_file,dic,nb_ligne):
 
     #Creation première ligne
     lst_arret = get_arret_ligne(data_file["regular_date_go"])
+    lst_arret_rv = get_arret_ligne(data_file["regular_date_back"])
 
     for i in range(0,len(lst_arret)):
 
@@ -26,9 +27,19 @@ def creation_arrets(data_file,dic,nb_ligne):
 
         if i+1 < len(lst_arret) :
             
-            dic.get(lst_arret[i]).add_lst_arret_suivant(dic.get(lst_arret[i+1]))
+            dic.get(lst_arret[i]).add_lst_arret_suivant(dic.get(lst_arret[i+1]),str(nb_ligne))
 
-    dic_ligne["Ligne" + str(nb_ligne)] = Lignes(dic.get(get_arret_ligne(data_file["regular_date_go"])[0]),Ligne_lst_Arret,nb_ligne)
+    for i in range(0,len(lst_arret_rv)):
+
+        if i+1 < len(lst_arret) :
+            
+            dic.get(lst_arret_rv[i]).add_lst_arret_suivant(dic.get(lst_arret_rv[i+1]),str(nb_ligne) + "_back")
+
+    dic_ligne["Ligne" + str(nb_ligne)] = Lignes(dic.get(get_arret_ligne(data_file["regular_date_go"])[0]),Ligne_lst_Arret,nb_ligne,"go")
+
+    Ligne_lst_Arret_rv= list(reversed(Ligne_lst_Arret))
+
+    dic_ligne["Ligne" + str(nb_ligne) +"_back"] = Lignes(dic.get(get_arret_ligne(data_file["regular_date_back"])[0]),Ligne_lst_Arret_rv,nb_ligne,"back")
 
     return { "dic_arret" : dic,
             "dic_ligne" : dic_ligne
