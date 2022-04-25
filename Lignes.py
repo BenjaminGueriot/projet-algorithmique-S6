f3252375ea72aa5432da17f607c41a2d2cc3fa93
BenjaminGueriot <gueriot.benjamin@gmail.com>
2022-04-25 20:47:57 +0200

    Class Lignes

--- Lignes.py
+++ Lignes.py
@@ -5,7 +5,7 @@ import Method
 
 class Lignes:
     
-    def __init__(self, ArretDepart,Lst_Arret,numero):
+    def __init__(self, ArretDepart,Lst_Arret,numero,direction):
         
         """
         Constructeur de la classe ArbreArret
@@ -17,9 +17,10 @@ class Lignes:
         Lst_Arret : Arret[]
             Liste des Arret de l'arbre      
         """
+
         self.numero = numero
         self.ArretDepart = ArretDepart
-        self.Lst_Arret = Lst_Arret
+        self.direction = direction
 
     def get_depart(self):
         return self.ArretDepart
@@ -29,7 +30,7 @@ class Lignes:
 
     def add_Lst_Arret(self,Arret):
         self.Lst_Arret.append(Arret)
-
+    """
     def get_all_path(self,dic_ligne,arretDebut,arretFin):
 
         count_route = 0
@@ -37,17 +38,18 @@ class Lignes:
         start = 0
         lst_routes = []
 
-        def dfs(forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes):
+        def sub(forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes):
             if forest:
                 ligne = forest[0]
+                
                 if(ligne.ArretDepart == arretDebut):
                     start = 1
-                if ligne.ArretDepart != arretFin and ligne.ArretDepart.is_leaf():
+                if (ligne.ArretDepart != arretFin) and (ligne.ArretDepart.is_leaf(ligne)):
                     new_forest = forest[1:]
-                    dfs(new_forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
+                    sub(new_forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
                 else :
                     if start == 1:
-                        lst_routes.append(ligne.ArretDepart.nom)
+                        lst_routes.append(ligne.ArretDepart.get_nom())
 
                     if ligne.ArretDepart == arretFin:
                         count_route += 1
@@ -55,15 +57,16 @@ class Lignes:
                         lst_routes = []
                         new_forest = forest[1:]
                     else :
-                        new_forest = [Lignes(arret, self.Lst_Arret,None) for arret in ligne.ArretDepart.get_lst_arret_suivant()] + forest[1:]
-                        if len(ligne.ArretDepart.get_lst_arret_suivant()) > 1:
+                        new_forest = [Lignes(arret, self.Lst_Arret,ligne.numero,ligne.direction) for arret in ligne.ArretDepart.get_lst_arret_suivant(ligne)] + forest[1:]
+                        if len(ligne.ArretDepart.get_lst_arret_suivant(ligne)) > 1:
                             pass
 
-                    dfs(new_forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
+                    sub(new_forest,dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
 
-        dfs([self],dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
+        sub([self],dic_ligne,count_route,arretDebut,arretFin,start,lst_routes)
         print(dic_route)
         return dic_route
+    """
 
     def nextBus(self,arret,timeDeparture,periode):
             
@@ -77,11 +80,28 @@ class Lignes:
 
     def time_between_arret(self,heure,arret1,periode):
        
-        if len(arret1.get_lst_arret_suivant()) == 1:
-            arret2 = arret1.get_lst_arret_suivant()[0]
-        else:
-            arret2 = arret1.get_lst_arret_suivant()[self.numero-1]
+        
+        arret2 = arret1.get_arret_suivant(self)
+
+        print(self.nextBus(arret1,heure,periode))
 
         time = self.nextBus(arret2,self.nextBus(arret1,heure,periode)+1,periode) - self.nextBus(arret1,heure,periode)
         
         return time
+
+    def findArret(self,arret):
+        res=self.ArretDepart
+        try:
+            while res != arret:
+                res = res.get_arret_suivant(self)
+            return res
+        except AttributeError:
+            return None
+
+    def isInLigne(self,arret):
+        try:
+             ifarret = self.findArret(arret)
+             return True
+        except:
+            return False
+    
