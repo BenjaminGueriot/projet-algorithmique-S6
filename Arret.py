64000376e721b218daf17ab13aee156e73c2d64e
BenjaminGueriot <gueriot.benjamin@gmail.com>
2022-04-26 15:03:02 +0200

    Class Arret

--- Arret.py
+++ Arret.py
@@ -62,35 +62,6 @@ class Arret :
         if(periode == "special_go"):
             return self.dic_horaire.get("Ligne"+ str(ligne)).get("special_go")
         if(periode == "special_back"):
-            print(self.dic_horaire.get("Ligne"+ str(ligne)).get("special_back"))
             return self.dic_horaire.get("Ligne"+ str(ligne)).get("special_back")
 
         return None
-    
-    def father(self,Ligne):
-
-        for n in Ligne.Lst_Arret:
-            if self in n.lst_arret_suivant  :
-                return n
-        return None
-
-    def is_leaf(self,ligne):
-
-        lst_arrets = []
-        if ligne.direction == "back":
-            for i in range(0,len(self.lst_arret_suivant.keys())):
-                if self.lst_arret_suivant.get(str((ligne.numero+i))+"_back"):
-                    lst_arrets.append(self.lst_arret_suivant.get(str((ligne.numero+i))+"_back"))
-            if lst_arrets == []:
-                return True
-            else:
-                return False
-
-        if ligne.direction == "go":
-            for i in range(0,len(self.lst_arret_suivant.keys())):
-                if self.lst_arret_suivant.get(str((ligne.numero+i))):
-                    lst_arrets.append(self.lst_arret_suivant.get(str((ligne.numero+i))))
-            if lst_arrets == []:
-                return True
-            else:
-                return False
