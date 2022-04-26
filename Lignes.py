531b3818a8f8aae535f98bf445b9c5b03beaa6e6
BenjaminGueriot <gueriot.benjamin@gmail.com>
2022-04-26 15:03:24 +0200

    Class Lignes

--- Lignes.py
+++ Lignes.py
@@ -1,5 +1,5 @@
 #-*-coding:utf-8-*-
-
+from math import inf
 from Arret import Arret
 import Method
 
@@ -30,47 +30,25 @@ class Lignes:
             
         count = 0
         i = 0
-        inittime = arret.get_horaire(periode,self.numero)[0]
-        while inittime == "-":
-            i += 1
-            inittime = arret.get_horaire(periode,self.numero)[i]
+        time = 0
+        if arret != None:
+            inittime = arret.get_horaire(periode,self.numero)[0]
+            while inittime == "-":
+                i += 1
+                inittime = arret.get_horaire(periode,self.numero)[i]
             
-        print(timeDeparture)
-        time = Method.hdigit2min(inittime)
-        while time < timeDeparture or count > len(arret.get_horaire(periode,self.numero)) or time == "-":
-            count += 1
-            if(arret.get_horaire(periode,self.numero)[count] != "-"):
-                time = Method.hdigit2min(arret.get_horaire(periode,self.numero)[count])
+            time = Method.hdigit2min(inittime)
+            while time < timeDeparture or count > len(arret.get_horaire(periode,self.numero)) or time == "-":
+                count += 1
+                if(arret.get_horaire(periode,self.numero)[count] != "-"):
+                    time = Method.hdigit2min(arret.get_horaire(periode,self.numero)[count])
 
-            
         return time
 
 
     def time_between_arret(self,heure,arret1,periode):
        
         arret2 = arret1.get_arret_suivant(self)
-        print(arret2)
-
-        print(self.nextBus(arret1,heure,periode))
-        print(self.nextBus(arret2,self.nextBus(arret1,heure,periode),periode))
         time = self.nextBus(arret2,self.nextBus(arret1,heure,periode),periode) - self.nextBus(arret1,heure,periode)
         
         return time
-
-
-    def findArret(self,arret):
-        res=self.ArretDepart
-        try:
-            while res != arret:
-                res = res.get_arret_suivant(self)
-            return res
-        except AttributeError:
-            return None
-"""
-    def isInLigne(self,arret):
-        try:
-             ifarret = self.findArret(arret)
-             return True
-        except:
-            return False
-"""
