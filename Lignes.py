174972fd56962fd4b8ec66f91eb4647ced27c265
BenjaminGueriot <gueriot.benjamin@gmail.com>
2022-04-27 02:21:43 +0200

    Class Ligne

--- Lignes.py
+++ Lignes.py
@@ -1,5 +1,6 @@
 #-*-coding:utf-8-*-
 from math import inf
+from time import time
 from Arret import Arret
 import Method
 
@@ -27,21 +28,26 @@ class Lignes:
         return self.ArretDepart
 
     def nextBus(self,arret,timeDeparture,periode):
-            
+
         count = 0
         i = 0
         time = 0
-        if arret != None:
+        if arret.get_horaire(periode,self.numero) != None:
             inittime = arret.get_horaire(periode,self.numero)[0]
             while inittime == "-":
                 i += 1
                 inittime = arret.get_horaire(periode,self.numero)[i]
             
             time = Method.hdigit2min(inittime)
-            while time < timeDeparture or count > len(arret.get_horaire(periode,self.numero)) or time == "-":
-                count += 1
+            while time < timeDeparture or count > len(arret.get_horaire(periode,self.numero)):
+                if count == len(arret.get_horaire(periode,self.numero)):
+                    break
+                
+                if arret.get_horaire(periode,self.numero)[count] == "-":
+                    time = timeDeparture
                 if(arret.get_horaire(periode,self.numero)[count] != "-"):
                     time = Method.hdigit2min(arret.get_horaire(periode,self.numero)[count])
+                count += 1
 
         return time
 
@@ -49,6 +55,25 @@ class Lignes:
     def time_between_arret(self,heure,arret1,periode):
        
         arret2 = arret1.get_arret_suivant(self)
-        time = self.nextBus(arret2,self.nextBus(arret1,heure,periode),periode) - self.nextBus(arret1,heure,periode)
+        print(arret1.get_nom(),arret2.get_nom())
+        time1 = self.nextBus(arret1,heure,periode)
+        time2 = self.nextBus(arret2,self.nextBus(arret1,heure,periode),periode)
+
+        time = time2 - time1
         
+        return time
+
+    def time_between_arrets(self,heure,arret1,arret2,periode):
+        
+        print(arret1.get_nom(),arret2.get_nom())
+        time1 = self.nextBus(arret1,heure,periode)
+        time2 = self.nextBus(arret2,self.nextBus(arret1,heure,periode),periode)
+
+        if time1 == 0:
+            time = 0
+            return time
+        
+        time = time2 - time1        
+              
+        print(Method.min2hdigit(time2),Method.min2hdigit(time1),time)
         return time
